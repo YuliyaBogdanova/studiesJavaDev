8ed9cfa0479ca53333b1c94b651cfd1bbc681927
YuliyaBogdanova <juliete.b25@gmail.com>
2025-02-20 21:11:19 +0300

    new Pets

--- src/by/bogdanova/L13_02_2025_2/Task00.java
+++ src/by/bogdanova/L13_02_2025_2/Task00.java
@@ -6,25 +6,16 @@ import java.util.List;
 public class Task00 {
     public static void main(String[] args) {
 
-//        Owner oner1 = new Owner();
-//        Owner owner2 = new Owner();
-//
-//        Pet cat = new Cat("Timmi", "cat", 5);
-//        Pet dog = new Dog("Oliver", "dog", 3);
-
         List<Pet> pets = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
             pets.add(PetFactory.next());
         }
 
-
-//        OwnewFactory.next().addPet(pet);
-//        OwnewFactory.next().addPet(pet2);
-//
-//        System.out.println(pet.toString());
-//        System.out.println(pet2.toString());
-
+        for (Pet pet: pets){
+            OwnewFactory.next().addPet(pet);
+            System.out.println(pet.toString());
+        }
 
         System.out.println("Общее количество домашних животных: " + Owner.getTotalOwners());
     }
